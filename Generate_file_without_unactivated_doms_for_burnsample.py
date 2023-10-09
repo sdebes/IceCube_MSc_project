7722dca8a34ead7368a8621c65d2cb99053295e5
sdebes <debes23@gmail.com>
2023-10-09 13:07:35 +0200

    Add files via upload

---
+++ Generate_file_without_unactivated_doms_for_burnsample.py
@@ -0,0 +1,179 @@
+"""
+This script takes event files for MC and data and returns them with added photon reconstruction columns.
+The added columns are:
+doca
+photon_distance
+photon_z
+photon_azimuth
+photon_zenith
+cherenkov_x
+cherenkov_y
+cherenkov_z
+dist_cher_A
+t_A
+
+One file of 100k events takes about 1.5-2 hours.
+"""
+
+print('\nScript started.')
+import numpy as np
+import pandas as pd
+import sqlite3
+import time
+
+#############################################
+
+rd_truth_path = "/groups/icecube/debes/storage/burnsample_predictions/14594307_stopped_muons_from_burnsample_with_pos_xyz_azimuth_zenith_reconstruction.parquet" #14.6m events from burnsample
+mc_truth_weighted_path = "/groups/icecube/debes/storage/stopped_muons_final_reco_MC_GBweighted.csv" #8.3M events
+savefolder = '/groups/icecube/debes/work/analyses/Efficiency/data/burnsample_reco_new_angle/'
+
+# DOM coordinates
+all_dom_coordinates = pd.read_csv('/groups/icecube/debes/work/analyses/Efficiency/data/5083_DOM_coordinates_with_homemade_dom_id_from_burnsample.csv', index_col=0)
+
+# Pulsemaps
+rd_db_path = "/groups/icecube/petersen/GraphNetDatabaseRepository/dev_lvl3_genie_burnsample/dev_lvl3_genie_burnsample_v5.db"
+mc_db_path = "/groups/icecube/petersen/GraphNetDatabaseRepository/Leon2022_DataAndMC_CSVandDB_StoppedMuons/last_one_lvl3MC.db"
+
+rd_truth = pd.read_parquet(rd_truth_path)
+mc_truth = pd.read_csv(mc_truth_weighted_path)
+############################################
+print('rd truth shape:', rd_truth.shape)
+print('mc truth shape:', mc_truth.shape)
+
+
+# Choose how many events to process
+N_events = int(8000)
+# Choose how many files the N_events events should be split into
+N_files = 1
+
+file_size = int(N_events/N_files)
+print('N_events:', N_events)
+print('file_size:', file_size)
+print('N_files:', N_files)
+
+#Always selecting the lowest N_events event_nos in the files for reproducibility
+rd_events = tuple(list(set(rd_truth['event_no'].sort_values().values[:N_events])))
+mc_events = tuple(list(set(mc_truth['event_no'].sort_values().values[:N_events])))
+
+############################################
+
+def find_photon_distance(DataFrame, truth = False):
+    df = DataFrame.copy()
+    index_of_refraction = 1.32 # Value used by AMANDA
+    cherenkov_angle = (90-40.75) * np.pi / 180 # = arccos(1/index_of_refraction) # 90-40.75 because 40.75 is the angle of the cone
+    c = 299792458 * 1e-9 # m/ns
+
+    if truth:
+        end_points = df[['position_x', 'position_y', 'position_z']].values.astype(np.float32)
+        azimuths   = df['azimuth'].values.astype(np.float32)
+        zeniths    = df['zenith'].values.astype(np.float32)
+    else:
+        end_points = df[['position_x_pred', 'position_y_pred', 'position_z_pred']].values.astype(np.float32)
+        azimuths   = df['azimuth_pred'].values.astype(np.float32)
+        zeniths    = df['zenith_pred'].values.astype(np.float32)
+
+    dom_points = df[['dom_x', 'dom_y', 'dom_z']].values.astype(np.float32)
+    dom_times  = df['dom_time'].values.astype(np.float32)
+    # Unit vectors in the direction the muon came from
+    tracks = np.column_stack((np.cos(azimuths) * np.sin(zeniths),
+                              np.sin(azimuths) * np.sin(zeniths),
+                              np.cos(zeniths)))
+
+    # Start points 1km away from the end points along the tracks
+    start_points = end_points + 2000 * tracks # Largest distance between two DOMs in the data is 1557m 
+    # Vectors going from the start points to the end points
+    
+    mu_vectors = end_points - start_points  # (R-Q)
+    A_DOM_vector = end_points - dom_points
+
+    projection_factor = np.sum(mu_vectors * A_DOM_vector, axis=1) / np.sum(mu_vectors ** 2, axis=1)
+    # Projection of the DOMs onto the tracks
+    pDoca = start_points - projection_factor[:, np.newaxis] * mu_vectors 
+    # Distance between the DOMs and the tracks
+    dist_Doca_DOM    = np.linalg.norm(dom_points - pDoca.astype(np.float32), axis=1) #
+    dist_Doca_Cher   = dist_Doca_DOM * np.tan(np.pi/2 - cherenkov_angle) #
+    cherenkov_points = pDoca - np.expand_dims(dist_Doca_Cher, axis=1) * mu_vectors / np.expand_dims(np.linalg.norm(mu_vectors, axis=1), axis=1)
+
+    photon_vector      = dom_points - cherenkov_points
+    photon_vector_norm = photon_vector / np.linalg.norm(photon_vector, axis=1)[:, np.newaxis]
+    photon_zeniths     = np.arccos(photon_vector_norm[:,2])
+    photon_azimuths    = np.arctan2(photon_vector_norm[:,1], photon_vector_norm[:,0])
+    photon_distances   = np.linalg.norm(dom_points - cherenkov_points, axis=1)
+    dist_cher_A        = np.linalg.norm(end_points - cherenkov_points, axis=1)
+    
+    t_A = dom_times - photon_distances * index_of_refraction / c + dist_cher_A/c
+
+    df['doca'] = dist_Doca_DOM
+    df['photon_distance'] = photon_distances
+    df['photon_z']        = np.mean([df['dom_z'], cherenkov_points[:,2]], axis=0)
+    df['photon_azimuth']  = photon_azimuths
+    df['photon_zenith']   = photon_zeniths
+    df['cherenkov_x']     = cherenkov_points[:,0]
+    df['cherenkov_y']     = cherenkov_points[:,1]
+    df['cherenkov_z']     = cherenkov_points[:,2]
+    df['dist_cher_A']     = dist_cher_A
+    df['t_A'] = t_A
+    return df
+
+############################################
+columns = ['charge', 'dom_time', 'dom_x', 'dom_y', 'dom_z', 'event_no', 'width']
+for iteration, j in enumerate(np.linspace(0, N_events-file_size, N_files)):
+    MIN, MAX = int(j), int(j+file_size)
+#    if iteration == 0:
+#        print('Events', MIN, 'to', MAX, 'skipped.')
+#        continue
+    t0 = time.time()
+
+    print('Processing file', iteration,' containing event', MIN, 'to', MAX)
+    
+    rd_selection = tuple([int(x) for x in list(rd_events)[MIN:MAX]])
+    conn = sqlite3.connect(rd_db_path)
+    rd_pulse = pd.read_sql_query("SELECT {} FROM SplitInIcePulses WHERE event_no in {}".format(", ".join(columns), rd_selection), conn)
+    conn.close()
+    print('rd_pulse shape:', rd_pulse.shape)
+    mc_selection = tuple([int(x) for x in list(mc_events)[MIN:MAX]])
+    conn = sqlite3.connect(mc_db_path)
+    mc_pulse = pd.read_sql_query("SELECT {} FROM SplitInIcePulses WHERE event_no in {}".format(", ".join(columns), mc_selection), conn)
+    conn.close()
+    
+    rounding = 2
+    rd_pulse[['dom_x', 'dom_y', 'dom_z']] = rd_pulse[['dom_x', 'dom_y', 'dom_z']].round(rounding)
+    mc_pulse[['dom_x', 'dom_y', 'dom_z']] = mc_pulse[['dom_x', 'dom_y', 'dom_z']].round(rounding)
+   
+    rd_pulse = pd.merge(rd_pulse, all_dom_coordinates, on=['dom_x', 'dom_y', 'dom_z'], how='inner')
+    print('rd_pulse shape after merge:', rd_pulse.shape)
+    mc_pulse = pd.merge(mc_pulse, all_dom_coordinates, on=['dom_x', 'dom_y', 'dom_z'], how='inner')
+    
+
+    mc = pd.merge(mc_pulse, mc_truth[['event_no', 'azimuth', 'azimuth_pred', 'zenith','zenith_pred','position_x','position_y', 'position_z', 'position_x_pred','position_y_pred', 'position_z_pred', 'GB_weights']], on=['event_no'], how='inner')
+    rd = pd.merge(rd_pulse, rd_truth[['event_no', 'azimuth_pred', 'zenith_pred', 'position_x_pred', 'position_y_pred', 'position_z_pred']], on=['event_no'], how='inner')
+    print('rd shape:', rd.shape)
+
+    t001 = time.time()
+    print('Events and pulsemaps loaded in:', int((t001-t0)/60), 'minutes.')
+
+    N = str(int(file_size/1000))
+    iteration_string = str(int(iteration))
+    
+    rd_part_done = find_photon_distance(rd)
+    print('rd_part_done shape:', rd_part_done.shape)
+    rd_part_done.to_parquet(savefolder + f'{N}k_rd_photon_reco_{iteration_string}.parquet')
+    t1 = time.time()
+    print('Data file completed in:', int((t001-t0)/60), 'minutes.')
+
+    mc_part_done = find_photon_distance(mc)
+
+    mc_part_done.to_parquet(savefolder + f'{N}k_mc_photon_reco_{iteration_string}.parquet')
+    t2 = time.time()
+    print('MC file completed in:', int((t2-t1)/60), 'minutes.')
+    
+    mc_truth_part_done = find_photon_distance(mc, truth=True)
+    mc_truth_part_done.to_parquet(savefolder + f'{N}k_mc_truth_photon_reco_{iteration_string}.parquet')
+    t3 = time.time()
+    print('MC truth file completed in:', int((t3-t2)/60), 'minutes.')
+    
+    print('Data file has the shape', rd_part_done.shape)
+    print('MC files have the shape', mc_part_done.shape)
+    print('Time used for the photon reconstruction of event', MIN,'to', MAX, ':', int((t3-t0)/60), 'minutes.')
+
+print('Script completed.')
